0ebcf69d9b45ec6a525a4f7ab473cf741cf637ea
William4954t <williamfrancootero@gmail.com>
2023-07-12 13:50:40 -0500

    funciona archivo

--- Mini5/src/ManejaArchivos.java
+++ Mini5/src/ManejaArchivos.java
@@ -1,23 +1,38 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Random;
 
 public class ManejaArchivos{
 
-    public void leerArchivo() throws IOException{
+    public void leerArchivos(){
         try {
             RandomAccessFile file = new RandomAccessFile("files\\archivo.txt","r");
             String linea = "";
             while((linea = file.readLine())!=null){
                 System.out.println(linea);
-
             }
+        
         } catch (FileNotFoundException e) {
             // TODO: handle exception
+            e.printStackTrace();
+        }catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
         }
+    }
 
-
-
-
+    public void estarArchivos(){
+        try {
+            RandomAccessFile file = new RandomAccessFile("files\\archivo.txt","rw");
+            file.seek(file.length());
+            file.writeBytes("\nSImon");
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
